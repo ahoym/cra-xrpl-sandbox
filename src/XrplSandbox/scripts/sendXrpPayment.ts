64a0c2bdcca6f840f6b49ade67beda67796efd8f
ahoym <colmahoy@gmail.com>
2022-01-27 03:12:34 -0500

    Move utilities and sendXrpPayment script
    First move towards deprecating ripple-lib usages

---
+++ src/XrplSandbox/scripts/sendXrpPayment.ts
@@ -0,0 +1,20 @@
+import { logMessageAndPass } from '../../utilities';
+import { xrplClient1, xrplClient2 } from '../index';
+
+/**
+ * For those that prefer to sandbox through script form.
+ * Import this file in the App.tsx file to have this executed on application load.
+ */
+
+// Generate testnet wallets
+const generateWalletRequestOne = xrplClient1
+  .generateWallet()
+  .then(logMessageAndPass('Created faucet wallet for Client 1'));
+const generateWalletRequestTwo = xrplClient2
+  .generateWallet()
+  .then(logMessageAndPass('Created faucet wallet for Client 2'));
+
+// After testnet wallet creations, send a 22 XRP payment
+Promise.all([generateWalletRequestOne, generateWalletRequestTwo])
+  .then(() => xrplClient1.sendPayment(22, xrplClient2.wallet()?.address!))
+  .then(logMessageAndPass('Sent transaction from Wallet 1 to Wallet 2'));
