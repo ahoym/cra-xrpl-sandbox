2f555c4eebc388de1aebf0c3d2d3185aee9d6a31
ahoym <colmahoy@gmail.com>
2022-02-02 08:05:04 -0500

    Abstract away acceptNftSellOffer

--- src/XrplSandbox/NFTokens/nftClient.ts
+++ src/XrplSandbox/NFTokens/nftClient.ts
@@ -3,6 +3,29 @@ import { Amount } from 'xrpl/dist/npm/models/common';
 import { StateRefProvider } from '../types';
 
 /**
+ * For cases where a buyer can accept a sell offer from a NFT owner.
+ * {@link https://xrpl.org/nftokenacceptoffer.html}
+ */
+export const acceptNftSellOffer = async (
+  stateRefProvider: StateRefProvider,
+  offerIndex: string,
+  brokerFee?: Amount
+): Promise<TxResponse> => {
+  const { client, wallet } = await stateRefProvider();
+  const acceptNftSellOfferPayload: NFTokenAcceptOffer = {
+    TransactionType: 'NFTokenAcceptOffer',
+    Account: wallet.address,
+    SellOffer: offerIndex,
+  };
+
+  if (brokerFee) {
+    acceptNftSellOfferPayload.BrokerFee = brokerFee;
+  }
+
+  return client.submitAndWait(acceptNftSellOfferPayload, { wallet });
+};
+
+/**
  * {@link https://xrpl.org/nftokenacceptoffer.html}
  */
 export const acceptNftBuyOffer = async (
