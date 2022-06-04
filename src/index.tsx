e0e6c8a3bf7b5b4a6cbcc6b7b8ed9b6f581da6fd
ahoym <colmahoy@gmail.com>
2022-06-04 14:36:46 -0400

    Upgrade react@18, typescript@4.7

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { XrplClientsProvider } from './contexts/XrplClientsContext';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <XrplClientsProvider>
       <App />
     </XrplClientsProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
