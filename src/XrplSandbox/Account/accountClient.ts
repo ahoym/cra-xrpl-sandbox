1dea1025e5acc34c8136b2fbdd9f906bbf7d182d
ahoym <colmahoy@gmail.com>
2022-07-15 06:59:40 -0400

    Autofill account related transactions before sign

--- src/XrplSandbox/Account/accountClient.ts
+++ src/XrplSandbox/Account/accountClient.ts
@@ -6,11 +6,11 @@ export const setAccount = async (
   accountSetTx: Partial<AccountSet>
 ) => {
   const { client, wallet } = await stateRefProvider();
-  const setAccountTxPayload: AccountSet = {
+  const setAccountTxPayload: AccountSet = await client.autofill({
     ...accountSetTx,
     TransactionType: 'AccountSet',
     Account: wallet.address,
-  };
+  });
   const signed = wallet.sign(setAccountTxPayload);
 
   return client.submitAndWait(signed.tx_blob);
@@ -22,12 +22,12 @@ export const deleteAccount = async (
   accountDeleteTx: Partial<AccountDelete>
 ) => {
   const { client, wallet } = await stateRefProvider();
-  const deleteAccountTxPayload: AccountDelete = {
+  const deleteAccountTxPayload: AccountDelete = await client.autofill({
     ...accountDeleteTx,
     TransactionType: 'AccountDelete',
     Account: wallet.address,
     Destination: destination,
-  };
+  });
   const signed = wallet.sign(deleteAccountTxPayload);
 
   return client.submitAndWait(signed.tx_blob);
