0fb1cf6a6a52d5f250d59a6b859a1ba11d7a6c11
ahoym <colmahoy@gmail.com>
2022-07-16 12:41:39 -0400

    Add script that creates offers through the DEX

---
+++ src/XrplSandbox/scripts/createOffer.ts
@@ -0,0 +1,64 @@
+import { logMessageAndPass } from '../../utilities';
+import { TEST_NET_EXPLORER } from '../constants';
+import { xrplClient1, xrplClient2 } from '../createClients';
+import {
+  issueCurrencyAndSetupTrustlineProcedure,
+  ISSUED_CURENCY_TOKEN,
+} from './issueCurrency';
+
+console.log('========🪙 Place order(s) script 🪙========');
+
+export const createCurrencyOfferFromReceiver =
+  issueCurrencyAndSetupTrustlineProcedure
+    .then(() =>
+      xrplClient2.createOffer({
+        // amount to sell
+        TakerGets: {
+          currency: ISSUED_CURENCY_TOKEN,
+          issuer: xrplClient1.wallet()!.address,
+          value: '100',
+        },
+        // amount to buy
+        TakerPays: 10,
+      })
+    )
+    .then(
+      logMessageAndPass(
+        `Created offer from Receiver for Issued Currency ${ISSUED_CURENCY_TOKEN}`
+      )
+    )
+    .then((response: any) => {
+      console.log(
+        `See TestNet explorer: ${TEST_NET_EXPLORER}accounts/${response.result.Account}`
+      );
+      return response;
+    });
+
+// place an order (or many orders) from receiver
+export const createCurrencyOfferFromIssuer =
+  issueCurrencyAndSetupTrustlineProcedure
+    .then(() =>
+      xrplClient1.createOffer({
+        // amount to buy
+        TakerGets: 10,
+        // amount to sell
+        TakerPays: {
+          currency: ISSUED_CURENCY_TOKEN,
+          issuer: xrplClient1.wallet()!.address,
+          value: '100',
+        },
+      })
+    )
+    .then(
+      logMessageAndPass(
+        `Created offer from Issuer for Issued Currency ${ISSUED_CURENCY_TOKEN}`
+      )
+    )
+    .then((response: any) => {
+      console.log(
+        `See TestNet explorer: ${TEST_NET_EXPLORER}accounts/${response.result.Account}`
+      );
+      return response;
+    });
+
+// TODO: make a 3rd account, Trustline to issuer, and place corresponding offers contra receiver
