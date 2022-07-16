025dde8b4b660c0f262459e07d48a5948ba647a8
ahoym <colmahoy@gmail.com>
2022-07-16 15:46:57 -0400

    Update issueCurrency script to be more generic

--- src/XrplSandbox/scripts/issueCurrency.ts
+++ src/XrplSandbox/scripts/issueCurrency.ts
@@ -1,18 +1,17 @@
 import { AccountSetAsfFlags } from 'xrpl';
 import { logMessageAndPass } from '../../utilities';
 import { xrplClient1, xrplClient2 } from '../createClients';
+import { XrplClient } from '../XrplClient';
+import { generateWallet } from './generateWallet';
 
 console.log('========🪙 Issue Currency script 🪙========');
 
 export const ISSUED_CURENCY_TOKEN = 'PLZ';
 const ISSUED_CURENCY_TOKEN_AMOUNT = '10000';
 
-const generateWalletRequestOne = xrplClient1 // Issuer
-  .generateWallet()
-  .then(logMessageAndPass('Created faucet wallet for Client 1'));
-const generateWalletRequestTwo = xrplClient2 // Receiver
-  .generateWallet()
-  .then(logMessageAndPass('Created faucet wallet for Client 2'));
+const generateWalletRequestOne = generateWallet(xrplClient1, {
+  clientDescription: 'Issuer',
+});
 
 const setRipplingForIssuerProcedure = generateWalletRequestOne
   .then(() =>
@@ -20,31 +19,54 @@ const setRipplingForIssuerProcedure = generateWalletRequestOne
   )
   .then(logMessageAndPass('Set default rippling for client 1'));
 
-const createTrustSetForReceiverProcedure = generateWalletRequestOne
-  .then(() => generateWalletRequestTwo)
-  .then(() =>
-    xrplClient2.setTrust({
-      limitAmount: {
-        issuer: xrplClient1.wallet()!.address,
-        currency: ISSUED_CURENCY_TOKEN,
-        value: ISSUED_CURENCY_TOKEN_AMOUNT,
-      },
-    })
-  )
-  .then(logMessageAndPass('Created Trustline between Issuer and Receiver'));
+export function createTrustSetForReceiver({
+  receiverClient,
+  issuerClient = xrplClient1,
+  receiverClientSecret,
+  issuerClientSecret,
+}: {
+  receiverClient: XrplClient;
+  issuerClient?: XrplClient;
+  receiverClientSecret?: string;
+  issuerClientSecret?: string;
+}): Promise<[XrplClient, XrplClient]> {
+  const issuerWalletPromise = generateWallet(issuerClient, {
+    clientDescription: 'Issuer',
+    fromSeed: issuerClientSecret,
+  });
+  const receiverWalletPromise = generateWallet(receiverClient, {
+    clientDescription: 'Receiver',
+    fromSeed: receiverClientSecret,
+  });
+
+  return Promise.all([issuerWalletPromise, receiverWalletPromise])
+    .then(([issuerWallet]) =>
+      receiverClient.setTrust({
+        limitAmount: {
+          issuer: issuerWallet.address,
+          currency: ISSUED_CURENCY_TOKEN,
+          value: ISSUED_CURENCY_TOKEN_AMOUNT,
+        },
+      })
+    )
+    .then(logMessageAndPass('Created Trustline between Issuer and Receiver'))
+    .then(() => [issuerClient, receiverClient]);
+}
 
 export const issueCurrencyAndSetupTrustlineProcedure = Promise.all([
   setRipplingForIssuerProcedure,
-  createTrustSetForReceiverProcedure,
+  createTrustSetForReceiver({
+    receiverClient: xrplClient2,
+  }),
 ])
-  .then(() =>
-    xrplClient1.sendPayment(
+  .then(([_, [issuerClient, receiverClient]]) =>
+    issuerClient.sendPayment(
       {
         currency: ISSUED_CURENCY_TOKEN,
-        issuer: xrplClient1.wallet()!.address,
+        issuer: issuerClient.wallet()!.address,
         value: '100',
       },
-      xrplClient2.wallet()!.address
+      receiverClient.wallet()!.address
     )
   )
   .then(
