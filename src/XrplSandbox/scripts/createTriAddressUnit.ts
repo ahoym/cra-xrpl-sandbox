4b2a48ff24ecbe6651401713493c52c9ad70d3e0
Malcolm Ahoy <mahoy@ripple.com>
2022-10-06 14:49:24 -0400

    Create working script for tri-address unit

--- src/XrplSandbox/scripts/createTriAddressUnit.ts
+++ src/XrplSandbox/scripts/createTriAddressUnit.ts
@@ -1,13 +1,15 @@
+import { AccountSetAsfFlags } from 'xrpl';
 import { logMessageAndPass } from '../../utilities';
 import {
-  generateDevNetXrplClient,
-  xrplDevNetClient1,
-  xrplDevNetClient2,
+  generateTestnetXrplClient,
+  xrplClient1,
+  xrplClient2,
 } from '../createClients';
 import {
   createTrustSetForReceiver,
   ISSUED_CURENCY_TOKEN,
 } from './createTrustSetForReceiver';
+import { generateWallet } from './generateWallet';
 
 console.log('========🪙 Create Tri Address Unit script 🪙========');
 
@@ -19,38 +21,56 @@ Nomenclature:
 - Issuing address (cold) - xrplClient1 🥶
 - Standby address (warm) - xrpClient2 😐
 - Operational address (hot) - xrpClient3 🔥
+
+General flow:
+
+1a. Create Trust set from Standby address (warm) to Issuing Address (cold)
+1b. Create Trust set from Operational address (hot) to Issuing Address (cold)
+2. Issue tokens to Standby (warm) address
+3. Refill tokens to Operational address
+4. Credit tokens to participants
+
+# TODO Authorized trustlines (both sides need to send TrustSet to each other)
 */
 
-const xrplDevNetClient3 = generateDevNetXrplClient();
+const xrplClient3 = generateTestnetXrplClient();
+(window as any).xrplClient3 = xrplClient3;
 
 // 1a. Create Trust set from Standby address (warm) to Issuing Address (cold)
 const standbyToIssuingTrustlinePromise = createTrustSetForReceiver({
-  issuerClient: xrplDevNetClient1,
-  issuerClientSecret: 'snVKpuNscB3RYzRbK7pL2W5kjM5Zo',
-  receiverClient: xrplDevNetClient2,
-  receiverClientSecret: 'shjw6wdSdcMNRBC4mWP3nW7hMQ8pH',
+  issuerClient: xrplClient1,
+  issuerClientSecret: '',
+  receiverClient: xrplClient2,
+  receiverClientSecret: '',
 });
+
+const setRipplingForIssuerProcedure = generateWallet(xrplClient1, {
+  clientDescription: 'Issuer',
+  fromSeed: '',
+})
+  .then(() =>
+    xrplClient1.setAccount({ SetFlag: AccountSetAsfFlags.asfDefaultRipple })
+  )
+  .then(logMessageAndPass('Set default rippling for Issuer Client'));
 
 // 1b. Create Trust set from Operational address (hot) to Issuing Address (cold)
 const operationalToIssuingTrustlinePromise = createTrustSetForReceiver({
-  issuerClient: xrplDevNetClient1,
-  issuerClientSecret: 'snVKpuNscB3RYzRbK7pL2W5kjM5Zo',
-  receiverClient: xrplDevNetClient3,
-  receiverClientSecret: 'spiPmtqDxNfwuhFSux5Hj6a4ih83W',
+  issuerClient: xrplClient1,
+  issuerClientSecret: '',
+  receiverClient: xrplClient3,
+  receiverClientSecret: '',
 });
 
-// 2. Issue tokens to Standby (warm) address
-
-// 3. Refill tokens to Operational address
-// 4. Credit tokens to participants
-
-Promise.all([
-  standbyToIssuingTrustlinePromise,
-  operationalToIssuingTrustlinePromise,
-])
+setRipplingForIssuerProcedure
+  .then(() =>
+    Promise.all([
+      standbyToIssuingTrustlinePromise,
+      operationalToIssuingTrustlinePromise,
+    ])
+  )
   .then(logMessageAndPass('Created both Trustlines to Issuing address'))
   .then(
-    async ([
+    ([
       [issuingClient1, standbyClient],
       [issuingClient2, operationalClient],
     ]) => {
@@ -66,20 +86,46 @@ Promise.all([
       console.log(
         `🔥 Operational client address ${operationalClient.wallet()!.address}`
       );
-
-      await issuingClient2.sendPayment(
-        {
-          currency: ISSUED_CURENCY_TOKEN,
-          issuer: issuingClient2.wallet()!.address,
-          value: '10000',
-        },
-        operationalClient.wallet()!.address
-      );
-      return [issuingClient2, operationalClient];
+      return [issuingClient1, standbyClient, operationalClient];
     }
   )
+  .then(async ([issuingClient, standbyClient, operationalClient]) => {
+    // 2. Issue tokens to Standby (warm) address
+    await issuingClient.sendPayment(
+      {
+        currency: ISSUED_CURENCY_TOKEN,
+        issuer: issuingClient.wallet()!.address,
+        value: '5000',
+      },
+      standbyClient.wallet()!.address
+    );
+    return [issuingClient, standbyClient, operationalClient];
+  })
   .then(
     logMessageAndPass(
-      `Sent Issued Currency ${ISSUED_CURENCY_TOKEN} to Receiver`
+      `Issued Currency ${ISSUED_CURENCY_TOKEN} to Standby client`
+    )
+  )
+  // 3. Refill tokens to Operational address
+  .then(async ([issuingClient, standbyClient, operationalClient]) => {
+    console.log(
+      'Step 3, refilling tokens from Standby client to Operational client'
+    );
+
+    await standbyClient.sendPayment(
+      {
+        currency: ISSUED_CURENCY_TOKEN,
+        issuer: issuingClient.wallet()!.address,
+        value: '500',
+      },
+      operationalClient.wallet()!.address
+    );
+
+    return [issuingClient, standbyClient, operationalClient];
+  })
+  .then(
+    logMessageAndPass(
+      `Sent ${ISSUED_CURENCY_TOKEN} from Standby client to Operational client`
     )
   );
+// 4. Credit tokens to participants
